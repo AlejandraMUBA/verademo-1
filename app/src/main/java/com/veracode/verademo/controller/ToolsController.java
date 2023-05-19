d94cadc647f78a92a5fde7874329cfabb262d63d
AlejandraMUBA <alejandra.munoz@cybolt.com>
2023-05-19 11:43:35 -0600

    SCA-Scan

--- app/src/main/java/com/veracode/verademo/controller/ToolsController.java
+++ app/src/main/java/com/veracode/verademo/controller/ToolsController.java
@@ -50,9 +50,9 @@ public class ToolsController {
 
 		try {
 			/* START EXAMPLE VULNERABILITY */
-			//proc = Runtime.getRuntime().exec(new String[] { "bash", "-c", "ping -c1 " + host });
+			proc = Runtime.getRuntime().exec(new String[] { "bash", "-c", "ping -c1 " + host });
 			/* END EXAMPLE VULNERABILITY */
-			proc = null;
+			//proc = null;
 			
 
 			proc.waitFor(5, TimeUnit.SECONDS);
